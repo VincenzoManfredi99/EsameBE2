d6abd6adc3d4c64a78d8d024a18dd49236414fa7
VincenzoManfredi99 <vincenzomanfredi2050@gmail.com>
2026-06-12 16:34:02 +0200

    Implementazione del metodo aggiungiGioco

--- src/main/java/vincenzomanfredi/Application.java
+++ src/main/java/vincenzomanfredi/Application.java
@@ -1,5 +1,6 @@
 package vincenzomanfredi;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,6 +31,7 @@ public class Application {
         while (scelta != 0) {
 
             System.out.println("MENU COLLEZIONE");
+            System.out.println("Premi 1 per aggiungere un gioco");
             System.out.println("Premi 2 per cercare un gioco tramite id");
             System.out.println("Premi 3 per cercare giochi in base al prezzo");
             System.out.println("Premi 4 per cercare giochi in base al numero di giocatori");
@@ -41,6 +43,66 @@ public class Application {
 
             switch (scelta) {
 
+                case 1:
+                    int sceltaAdd;
+                    System.out.println("Premi 1 per aggiungere un Videogioco o 2 per aggiungere un gioco da tavolo");
+                    sceltaAdd = tastiera.nextInt();
+
+                    if (sceltaAdd != 1 && sceltaAdd != 2) {
+                        System.out.println("Opzione non valida");
+                        return;
+                    }
+
+                    int id = 0;
+                    String titolo = "";
+                    double prezzo = 0;
+                    int annoUscita = 0;
+
+                    System.out.println("Inserisci un id: ");
+                    id = tastiera.nextInt();
+                    tastiera.nextLine();  //Titolo veniva saltato andando a prezzo
+                    System.out.println("Inserisci un titolo: ");
+                    titolo = tastiera.nextLine();
+                    System.out.println("Inserisci un prezzo: ");
+                    prezzo = tastiera.nextDouble();
+                    System.out.println("Inserisci l'anno d'uscita: ");
+                    annoUscita = tastiera.nextInt();
+
+
+                    if (sceltaAdd == 1) {
+                        String inputPiattaforma = "";
+                        List<String> piattaforma = Arrays.asList(inputPiattaforma.split(",")); //taglia la stringa ad ogni virgola dando come risultato un array di stringhe come vuole l'attributo piattaforma
+                        String inputGeneri = "";
+                        int durata = 0;
+                        System.out.println("Inserisci la piattaforma di gioco: ");
+                        inputPiattaforma = tastiera.nextLine();
+                        System.out.println("Inserisci la durata del gioco: ");
+                        durata = tastiera.nextInt();
+                        System.out.println("Inserisci il genere del gioco: ");
+                        inputGeneri = tastiera.nextLine();
+                        Genere genere = Genere.valueOf(inputGeneri);
+
+
+                    } else {
+                        int nGiocatori = 0;
+                        int durataPartita = 0;
+                        System.out.println("Inserisci il numero di giocatori di questo gioco da tavolo: ");
+                        nGiocatori = tastiera.nextInt();
+                        System.out.println("Inserisci la durata media di una partita: ");
+                        durataPartita = tastiera.nextInt();
+
+                        BoardGame nuovoBoardGame = new BoardGame(id, titolo, annoUscita, prezzo, nGiocatori, durataPartita);
+
+                        try {
+                            miaCollezione.aggiungiGioco(nuovoBoardGame);
+                            System.out.println("Gioco aggiunto con successo!");
+                        } catch (Exception e) {
+                            System.out.println("Si è verificato un problema: " + e.getMessage());
+                        }
+
+                    }
+                    break;
+
                 case 2:
                     System.out.println("Inserisci l'id da cercare: ");
                     int idCercato = tastiera.nextInt();
@@ -51,6 +113,7 @@ public class Application {
                         System.out.println("Si è verificato un problema:" + e.getMessage());
                     }
                     break;
+
                 case 3:
                     System.out.println("Inserisci un prezzo per filtrare i giochi: ");
                     double prezzoMassimo = tastiera.nextDouble();
